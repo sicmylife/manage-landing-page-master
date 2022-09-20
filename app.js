66c45a3554672e71293b2bbded779a87b2c76fe6
Daniel <sicmylife@gmail.com>
2022-09-20 17:59:25 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// const productContainers = [...document.querySelectorAll('.product-container')];
+// const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+// const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+// productContainers.forEach((item, i) => {
+//     let containerDimensions = item.getBoundingClientRect();
+//     let containerWidth = containerDimensions.width;
+//     let containerRight = containerDimensions.height;
+
+//     nxtBtn[i].addEventListener('click', () => {
+     
+//         item.scrollLeft += containerWidth ;
+
+//     })
+
+//     preBtn[i].addEventListener('click', () => {
+//         item.scrollLeft -= containerWidth;
+//     })
+// })
+
+ 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+  console.log('Fastest');
+  
